38319acd57800049f170e1c6ba768d29b07cfc71
Hareesha Marayyagari <hareeshareddy0301@gmail.com>
2023-08-14 21:02:13 -0700

    Modified postgress credentials

--- moviebuff/settings.py
+++ moviebuff/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'moviebuff.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER':'postgres',
+        'NAME': 'mydb',
+        'USER':'myuser',
+        'PASSWORD': 'mypassword',
         'HOST':'db',
         'PORT':5432
     }
